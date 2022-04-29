bbc4448e9fa91327ab16adb31337fff8d41c50d9
DGBratoevski20 <DGBratoevski20@codingburgas.bg>
2022-04-29 23:10:59 +0300

    Finish middle cards

--- Vis!00/Vis!00/Vis!00.cpp
+++ Vis!00/Vis!00/Vis!00.cpp
@@ -33,21 +33,25 @@ int main(void)
         DrawText("5", 1060, 350, 150, BLACK);
         DrawRectangle((int)colorsRecs.x + 1150, (int)colorsRecs.y - 110, (int)colorsRecs.width + 150, (int)colorsRecs.height + 250, ORANGE);
         DrawText("6", 1470, 350, 150, BLACK);
-        
+         
+
         if (a == 3)
         {
             DrawRectangle((int)colorsRecs.x + 360, (int)colorsRecs.y, (int)colorsRecs.width, (int)colorsRecs.height, DARKGRAY);
             DrawRectangle((int)colorsRecs.x + 360, (int)colorsRecs.y + 72, (int)colorsRecs.width, (int)colorsRecs.height - 142.5, WHITE);
-            DrawText("1", 630, 353, 75, BLACK);
-            DrawText("0", 620, 427.5, 75, BLACK);
             DrawRectangle((int)colorsRecs.x + 610, (int)colorsRecs.y, (int)colorsRecs.width, (int)colorsRecs.height, DARKGRAY);
             DrawRectangle((int)colorsRecs.x + 610, (int)colorsRecs.y + 72, (int)colorsRecs.width, (int)colorsRecs.height - 142.5, WHITE);
-            DrawText("0", 875, 353, 75, BLACK);
-            DrawText("1", 880.5, 427.5, 75, BLACK);
             DrawRectangle((int)colorsRecs.x + 860, (int)colorsRecs.y, (int)colorsRecs.width, (int)colorsRecs.height, DARKGRAY);
             DrawRectangle((int)colorsRecs.x + 860, (int)colorsRecs.y + 72, (int)colorsRecs.width, (int)colorsRecs.height - 142.5, WHITE);
-            DrawText("1", 1130, 353, 75, BLACK);
-            DrawText("0", 1120, 427.5, 75, BLACK);
+           
+                DrawText("0", 625, 353, 75, BLACK);
+                DrawText("1", 630, 427.5, 75, BLACK);
+                DrawText("0", 875, 353, 75, BLACK);
+                DrawText("1", 880.5, 427.5, 75, BLACK);
+                DrawText("1", 1130, 353, 75, BLACK);
+                DrawText("0", 1120, 427.5, 75, BLACK);
+                
+            
         }
         else if (a == 4)
         {
@@ -71,19 +75,52 @@ int main(void)
         else if (a == 5)
         {
             DrawRectangle((int)colorsRecs.x + 85, (int)colorsRecs.y, (int)colorsRecs.width, (int)colorsRecs.height, DARKGRAY);
+            DrawRectangle((int)colorsRecs.x + 85, (int)colorsRecs.y + 72, (int)colorsRecs.width, (int)colorsRecs.height - 142.5, WHITE);
+            DrawText("0", 350, 353, 75, BLACK);
+            DrawText("1", 357.5, 427.5, 75, BLACK);
             DrawRectangle((int)colorsRecs.x + 335, (int)colorsRecs.y, (int)colorsRecs.width, (int)colorsRecs.height, DARKGRAY);
+            DrawRectangle((int)colorsRecs.x + 335, (int)colorsRecs.y + 72, (int)colorsRecs.width, (int)colorsRecs.height - 142.5, WHITE);
+            DrawText("1", 604, 353, 75, BLACK);
+            DrawText("0", 597, 427.5, 75, BLACK);
             DrawRectangle((int)colorsRecs.x + 585, (int)colorsRecs.y, (int)colorsRecs.width, (int)colorsRecs.height, DARKGRAY);
+            DrawRectangle((int)colorsRecs.x + 585, (int)colorsRecs.y + 72, (int)colorsRecs.width, (int)colorsRecs.height - 142.5, WHITE);
+            DrawText("1", 856.5, 353, 75, BLACK);
+            DrawText("0", 848.9, 427.5, 75, BLACK);
             DrawRectangle((int)colorsRecs.x + 835, (int)colorsRecs.y, (int)colorsRecs.width, (int)colorsRecs.height, DARKGRAY);
+            DrawRectangle((int)colorsRecs.x + 835, (int)colorsRecs.y + 72, (int)colorsRecs.width, (int)colorsRecs.height - 142.5, WHITE);
+            DrawText("0", 1098, 353, 75, BLACK);
+            DrawText("1", 1106, 427.5, 75, BLACK);
             DrawRectangle((int)colorsRecs.x + 1085, (int)colorsRecs.y, (int)colorsRecs.width, (int)colorsRecs.height, DARKGRAY);
+            DrawRectangle((int)colorsRecs.x + 1085, (int)colorsRecs.y + 72, (int)colorsRecs.width, (int)colorsRecs.height - 142.5, WHITE);
+            DrawText("1", 1355, 353, 75, BLACK);
+            DrawText("0", 1349, 427.5, 75, BLACK);
         }
         else if (a == 6)
         {
             DrawRectangle((int)colorsRecs.x, (int)colorsRecs.y, (int)colorsRecs.width, (int)colorsRecs.height, DARKGRAY);
-            DrawRectangle((int)colorsRecs.x + 50 + 50 + 50 + 50 + 50, (int)colorsRecs.y, (int)colorsRecs.width, (int)colorsRecs.height, DARKGRAY);
+            DrawRectangle((int)colorsRecs.x, (int)colorsRecs.y + 72, (int)colorsRecs.width, (int)colorsRecs.height - 142.5, WHITE);
+            DrawText("0", 266, 353, 75, BLACK);
+            DrawText("1", 271.5, 427.5, 75, BLACK);
+            DrawRectangle((int)colorsRecs.x + 250, (int)colorsRecs.y, (int)colorsRecs.width, (int)colorsRecs.height, DARKGRAY);
+            DrawRectangle((int)colorsRecs.x + 250, (int)colorsRecs.y + 72, (int)colorsRecs.width, (int)colorsRecs.height - 142.5, WHITE);
+            DrawText("1", 524.5, 353, 75, BLACK);
+            DrawText("0", 512, 427.5, 75, BLACK);
             DrawRectangle((int)colorsRecs.x + 500, (int)colorsRecs.y, (int)colorsRecs.width, (int)colorsRecs.height, DARKGRAY);
+            DrawRectangle((int)colorsRecs.x + 500, (int)colorsRecs.y + 72, (int)colorsRecs.width, (int)colorsRecs.height - 142.5, WHITE);
+            DrawText("1", 767.5, 353, 75, BLACK);
+            DrawText("0", 761.5, 427.5, 75, BLACK);
             DrawRectangle((int)colorsRecs.x + 750, (int)colorsRecs.y, (int)colorsRecs.width, (int)colorsRecs.height, DARKGRAY);
+            DrawRectangle((int)colorsRecs.x + 750, (int)colorsRecs.y + 72, (int)colorsRecs.width, (int)colorsRecs.height - 142.5, WHITE);
+            DrawText("0", 1010.5, 353, 75, BLACK);
+            DrawText("1", 1022.5, 427.5, 75, BLACK);
             DrawRectangle((int)colorsRecs.x + 1000, (int)colorsRecs.y, (int)colorsRecs.width, (int)colorsRecs.height, DARKGRAY);
+            DrawRectangle((int)colorsRecs.x + 1000, (int)colorsRecs.y + 72, (int)colorsRecs.width, (int)colorsRecs.height - 142.5, WHITE);
+            DrawText("1", 1270, 353, 75, BLACK);
+            DrawText("0", 1264, 427.5, 75, BLACK);
             DrawRectangle((int)colorsRecs.x + 1250, (int)colorsRecs.y, (int)colorsRecs.width, (int)colorsRecs.height, DARKGRAY);
+            DrawRectangle((int)colorsRecs.x + 1250, (int)colorsRecs.y + 72, (int)colorsRecs.width, (int)colorsRecs.height - 142.5, WHITE);
+            DrawText("0", 1513.5, 353, 75, BLACK);
+            DrawText("1", 1525, 427.5, 75, BLACK);
         }
         ClearBackground(LIGHTGRAY);
         
