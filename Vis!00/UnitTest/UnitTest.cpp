59e55782e17a8cc14934645d0a5591cfd8a12916
DGBratoevski20 <DGBratoevski20@codingburgas.bg>
2022-05-12 08:00:42 +0300

    Add automized QA testing

---
+++ Vis!00/UnitTest/UnitTest.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// AAA
+			// arrange   -   act  -   assert
+
+			// arrange
+			int result;
+
+
+			// act
+			
+
+			// assert
+			Assert::AreEqual(451, result);
+
+
+		}
+	};
+}
