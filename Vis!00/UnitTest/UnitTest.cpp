cfe81fb7edbc066e4bed1d3e7f475cc279dfdfc5
Denislav Bratoevski <dgbratoevski20@codingburgas.bg>
2022-05-14 13:43:32 +0300

    Add almost finished QA testing and reconfigure SetUpPyramidCard header file

--- Vis!00/UnitTest/UnitTest.cpp
+++ Vis!00/UnitTest/UnitTest.cpp
@@ -1,5 +1,6 @@
+#pragma once
 #include "pch.h"
-#include "CppUnitTest.h"
+#include "../Vis!00/SetupBaseCardsPyramid.h"
 
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -9,8 +10,8 @@ namespace UnitTest
 	TEST_CLASS(UnitTest)
 	{
 	public:
-		
-		TEST_METHOD(Test_If_var1_value_is_right)
+
+		TEST_METHOD(Should_return_true_if_var1_value_is_right)
 		{
 			// AAA
 			// arrange   -   act  -   assert
@@ -20,32 +21,43 @@ namespace UnitTest
 
 
 			// act
-			
+			result = checkPyramidPositions(725);
 
 			// assert
-			Assert::AreEqual(451, result);
+			Assert::AreEqual(result, L"Expected true!");
 
 
 		}
-		TEST_METHOD(Test_If_var2_value_is_correct)
+		TEST_METHOD(Should_return_true_if_var2_value_is_right)
 		{
-
+			int result;
+			result = checkPyramidPositions2(830);
+			Assert::AreEqual(result, L"Expected true!");
 		}
 		TEST_METHOD(Test_If_var3_value_is_correct)
 		{
-
+			int result;
+			result = checkPyramidPositions(540);
+			Assert::AreEqual(result, L"Expected true!");
 		}
-		TEST_METHOD(Test_If_var4_value_is_correct)
+		TEST_METHOD(Should_return_true_if_var4_value_is_right)
 		{
-
+			int result;
+			result = checkPyramidPositions3(670);
+			Assert::AreEqual(result, L"Expected true!");
 		}
-		TEST_METHOD(Test_If_var5_value_is_correct)
+		TEST_METHOD(Should_return_true_if_var5_value_is_right)
 		{
-
+			int result;
+			result = checkPyramidPositions4(640);
+			Assert::AreEqual(result, L"Expected true!");
 		}
-		TEST_METHOD(Test_If_var6_value_is_correct)
+		
+		TEST_METHOD(Should_return_false_if_var6_value_is_not_right)
 		{
-
+			int result;
+			result = checkPyramidPositions5(6);
+			Assert::AreNotEqual(result, L"Expected false!");
 		}
 	};
 }
--- Vis!00/Vis!00/Vis!00.cpp
+++ Vis!00/Vis!00/Vis!00.cpp
@@ -1,5 +1,323 @@
 #include "Main.h"
-#include "SetupBaseCardsPyramid.h"
+bool checkPyramidPositions(int checker)
+{
+    return (checker==725);
+}
+bool checkPyramidPositions2(int checker)
+{
+    return (checker == 830);
+}
+bool checkPyramidPositions3(int checker)
+{
+    return (checker == 540);
+}
+bool checkPyramidPositions4(int checker)
+{
+    return (checker == 670);
+}
+bool checkPyramidPositions5(int checker)
+{
+    return (checker == 640);
+}
+bool checkPyramidPositions6(int checker)
+{
+    return (checker == 740);
+}
+
+void SetupDown(vector<Rectangle>& basePyramidcard)
+{
+    int len = basePyramidcard.size();
+    int counter = 0;
+    int var1 = 725;
+    int var2 = 830;
+    int var3 = 540;
+    int var4 = 670;
+    int var5 = 640;
+    int var6 = 740;
+    //vars for qa tests
+    switch (len)
+    {
+    case 3://if you click the card with 3
+    {     int i = 0;
+    for (i; i < 2; i++)
+    {
+        basePyramidcard[i].x = var1 + i * 210;
+        basePyramidcard[i].y = var3;
+        basePyramidcard[i].width = 99.5;
+        basePyramidcard[i].height = 125;
+    }
+    if (i == 2)
+    {
+        basePyramidcard[i].x = var2;
+        basePyramidcard[i].y = var4;
+        basePyramidcard[i].width = 99.5;
+        basePyramidcard[i].height = 125;
+    }
+    break;
+    }
+
+    case 6: //if you click the card with 4
+    {
+        int i = 0;
+        for (i; i < 3; i++)
+        {
+            basePyramidcard[i].x = var5 + i * 200;
+            basePyramidcard[i].y = 540;
+            basePyramidcard[i].width = 99.5;
+            basePyramidcard[i].height = 125;;
+        }
+        for (i; i < 5; i++)
+        {
+            basePyramidcard[i].x = var6 + counter * 200;
+            basePyramidcard[i].y = 670;
+            basePyramidcard[i].width = 99.5;
+            basePyramidcard[i].height = 125;
+            counter++;
+        }
+        if (i == 5)
+        {
+            basePyramidcard[i].x = 840;
+            basePyramidcard[i].y = 800;
+            basePyramidcard[i].width = 99.5;
+            basePyramidcard[i].height = 125;
+        }
+        break;
+    }
+
+    case 10: //if you click the card with 5
+    {
+        int i = 0;
+        for (i; i < 4; i++)
+        {
+            basePyramidcard[i].x = 555 + i * 200;
+            basePyramidcard[i].y = 550;
+            basePyramidcard[i].width = 80;
+            basePyramidcard[i].height = 100;
+        }
+        for (i; i < 7; i++)
+        {
+            basePyramidcard[i].x = 650 + counter * 200;
+            basePyramidcard[i].y = 660;
+            basePyramidcard[i].width = 80;
+            basePyramidcard[i].height = 100;
+            counter++;
+        }
+        counter = 0;
+        for (i; i < 9; i++)
+        {
+            basePyramidcard[i].x = 750 + counter * 200;
+            basePyramidcard[i].y = 770;
+            basePyramidcard[i].width = 80;
+            basePyramidcard[i].height = 100;
+            counter++;
+        }
+        counter = 0;
+        if (i == 9)
+        {
+            basePyramidcard[i].x = 850;
+            basePyramidcard[i].y = 880;
+            basePyramidcard[i].width = 80;
+            basePyramidcard[i].height = 100;
+        }
+        break;
+    }
+
+    case 15: //if you click the card with 6
+    {
+        int i = 0;
+        for (i; i < 5; i++)
+        {
+            basePyramidcard[i].x = 465 + i * 200;
+            basePyramidcard[i].y = 420 + 120;
+            basePyramidcard[i].width = 70;
+            basePyramidcard[i].height = 70;
+        }
+        for (i; i < 9; i++)
+        {
+            basePyramidcard[i].x = 565 + counter * 200;
+            basePyramidcard[i].y = 420 + 120 + 80;
+            basePyramidcard[i].width = 70;
+            basePyramidcard[i].height = 70;
+            counter++;
+        }
+        counter = 0;
+        for (i; i < 12; i++)
+        {
+            basePyramidcard[i].x = 665 + counter * 200;
+            basePyramidcard[i].y = 420 + 120 + 80 + 80;
+            basePyramidcard[i].width = 70;
+            basePyramidcard[i].height = 70;
+            counter++;
+        }
+        counter = 0;
+        for (i; i < 14; i++)
+        {
+            basePyramidcard[i].x = 765 + counter * 200;
+            basePyramidcard[i].y = 420 + 120 + 80 + 80 + 80;
+            basePyramidcard[i].width = 70;
+            basePyramidcard[i].height = 70;
+            counter++;
+        }
+        counter = 0;
+        if (i == 14)
+        {
+            basePyramidcard[i].x = 865;
+            basePyramidcard[i].y = 420 + 120 + 80 + 80 + 80 + 80;
+            basePyramidcard[i].width = 70;
+            basePyramidcard[i].height = 70;
+        }
+        break;
+
+    }
+
+
+    }
+
+}
+void SetupUp(vector<Rectangle>& basePyramidcard)
+{
+    int len = basePyramidcard.size();
+    int counter = 0;
+    switch (len)
+    {
+    case 3://if you click the card with 3
+    {     int i = 0;
+    for (i; i < 2; i++)
+    {
+        basePyramidcard[i].x = 725 + i * 210;
+        basePyramidcard[i].y = 280;
+        basePyramidcard[i].width = 99.5;
+        basePyramidcard[i].height = 125;
+    }
+    if (i == 2)
+    {
+        basePyramidcard[i].x = 750 + 80;
+        basePyramidcard[i].y = 150;
+        basePyramidcard[i].width = 99.5;
+        basePyramidcard[i].height = 125;
+    }
+    break;
+    }
+
+    case 6: //if you click the card with 4
+    {
+        int i = 0;
+        for (i; i < 3; i++)
+        {
+            basePyramidcard[i].x = 640 + i * 200;
+            basePyramidcard[i].y = 280;
+            basePyramidcard[i].width = 99.5;
+            basePyramidcard[i].height = 125;
+        }
+        for (i; i < 5; i++)
+        {
+            basePyramidcard[i].x = 740 + counter * 200;
+            basePyramidcard[i].y = 150;
+            basePyramidcard[i].width = 99.5;
+            basePyramidcard[i].height = 125;
+            counter++;
+        }
+        if (i == 5)
+        {
+            basePyramidcard[i].x = 840;
+            basePyramidcard[i].y = 20;
+            basePyramidcard[i].width = 99.5;
+            basePyramidcard[i].height = 125;
+        }
+        break;
+    }
+
+    case 10: //if you click the card with 5
+    {
+        int i = 0;
+        for (i; i < 4; i++)
+        {
+            basePyramidcard[i].x = 555 + i * 200;
+            basePyramidcard[i].y = 330;
+            basePyramidcard[i].width = 80;
+            basePyramidcard[i].height = 100;
+        }
+        for (i; i < 7; i++)
+        {
+            basePyramidcard[i].x = 650 + counter * 200;
+            basePyramidcard[i].y = 222.5;
+            basePyramidcard[i].width = 80;
+            basePyramidcard[i].height = 100;
+            counter++;
+        }
+        counter = 0;
+        for (i; i < 9; i++)
+        {
+            basePyramidcard[i].x = 750 + counter * 200;
+            basePyramidcard[i].y = 115;
+            basePyramidcard[i].width = 80;
+            basePyramidcard[i].height = 100;
+            counter++;
+        }
+        counter = 0;
+        if (i == 9)
+        {
+            basePyramidcard[i].x = 850;
+            basePyramidcard[i].y = 7.5;
+            basePyramidcard[i].width = 80;
+            basePyramidcard[i].height = 100;
+        }
+        break;
+    }
+
+    case 15: //if you click the card with 6
+    {
+        int i = 0;
+        for (i; i < 5; i++)
+        {
+            basePyramidcard[i].x = 465 + i * 200;
+            basePyramidcard[i].y = 420 - 90;
+            basePyramidcard[i].width = 70;
+            basePyramidcard[i].height = 70;
+        }
+        for (i; i < 9; i++)
+        {
+            basePyramidcard[i].x = 565 + counter * 200;
+            basePyramidcard[i].y = 420 - 90 - 80;
+            basePyramidcard[i].width = 70;
+            basePyramidcard[i].height = 70;
+            counter++;
+        }
+        counter = 0;
+        for (i; i < 12; i++)
+        {
+            basePyramidcard[i].x = 665 + counter * 200;
+            basePyramidcard[i].y = 420 - 90 - 80 - 80;
+            basePyramidcard[i].width = 70;
+            basePyramidcard[i].height = 70;
+            counter++;
+        }
+        counter = 0;
+        for (i; i < 14; i++)
+        {
+            basePyramidcard[i].x = 765 + counter * 200;
+            basePyramidcard[i].y = 420 - 90 - 80 - 80 - 80;
+            basePyramidcard[i].width = 70;
+            basePyramidcard[i].height = 70;
+            counter++;
+        }
+        counter = 0;
+        if (i == 14)
+        {
+            basePyramidcard[i].x = 865;
+            basePyramidcard[i].y = 420 - 90 - 80 - 80 - 80 - 80;
+            basePyramidcard[i].width = 70;
+            basePyramidcard[i].height = 70;
+        }
+        break;
+
+    }
+
+
+    }
+
+}
+
 void Filled(vector<Rectangle>& baseDown, vector<Rectangle>& baseUp, Rectangle& movingRed, Rectangle& movingBlue, Rectangle& movingYellow, Rectangle& movingF, Rectangle& movingRedRight, Rectangle& movingBlueRight, Rectangle& movingYellowRight, Rectangle& movingFRight, Rectangle& movingFive, Rectangle& movingFiveRight, vector<bool>& filledDown, vector<bool>& filledUp, Rectangle OperationArr[], vector<string>& checkDir, string randDir[], vector<string>& checkDirTwo)
 {
     checkDir.resize(filledDown.size());
